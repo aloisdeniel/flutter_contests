98032833d0460356b3d79323757fcb5c7c134867
Aloïs Deniel <alois.deniel@gmail.com>
2019-03-02 20:21:39 +0100

    Improved naming, removed unnecessary strokeJoin.

--- 2019_create/create_run/lib/main.dart
+++ 2019_create/create_run/lib/main.dart
@@ -9,36 +9,37 @@ import 'hooks.dart';
 h(f) => HookBuilder(builder: (c) => f(c) as Widget);
 c(v) => Color(v);
 du(m) => Duration(milliseconds: m);
-ic(i, c) => Icon(i, color: c);
-ei(v) => EdgeInsets.all(v ?? 0);
-of([x = 0.0, y = 0.0]) => Offset(x, y);
-lb(t, s, c, w) => Text(t,
+ico(i, c) => Icon(i, color: c);
+inset(v) => EdgeInsets.all(v ?? 0);
+off([x = 0.0, y = 0.0]) => Offset(x, y);
+txt(t, s, c, w) => Text(t,
     textAlign: TextAlign.center,
     style: TextStyle(color: c, fontSize: s, fontWeight: FontWeight.values[w]));
-sp() => SizedBox(height: m1, width: m2 / 2);
+sb(w, h, [c]) => SizedBox(height: w, width: h, child: c);
 flex(w, [d = 0, m = 2, c = 2, l = 1]) => Flex(
     direction: Axis.values[d],
     mainAxisAlignment: MainAxisAlignment.values[m],
     crossAxisAlignment: CrossAxisAlignment.values[c],
     textDirection: TextDirection.values[l],
     children: w.cast<Widget>());
-cr(c, [i, m, h, w, b, r]) => Container(
-    margin: ei(m),
-    padding: ei(i),
+box(c, [i, m, h, w, b, r]) => Container(
+    margin: inset(m),
+    padding: inset(i),
     decoration:
         BoxDecoration(borderRadius: BorderRadius.circular(r ?? 0), color: b),
     height: h,
     width: w,
     child: c);
 tr(o, x, w) =>
-    Opacity(opacity: o, child: Transform.translate(offset: of(x), child: w));
+    Opacity(opacity: o, child: Transform.translate(offset: off(x), child: w));
 clerp(t) => Color.lerp(blue, green, t);
-sca(b, i, t, w) => Scaffold(
+scaff(b, i, t, w) => Scaffold(
     backgroundColor: b,
     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     floatingActionButton:
-        FloatingActionButton(child: ic(i, dark), onPressed: t),
+        FloatingActionButton(child: ico(i, dark), onPressed: t),
     body: w);
+var sp = sb(m1, m2 / 2);
 var io = Curves.easeInOut;
 var m1 = 24.0;
 var m2 = 12.0;
@@ -51,21 +52,21 @@ var halo = (c, r, x, y, s) => RadialGradient(
         colors: [c, c.withAlpha(0)],
         radius: r / s.height,
         center: Alignment((x - 0.5) * 2, (y - 0.5) * 2))
-    .createShader(of() & s);
+    .createShader(off() & s);
 main() => runApp(MaterialApp(theme: ThemeData.dark(), home: home()));
 run(c) => h((c) {
       final sm = useMemoized(() => Stream.periodic(du(1000), (i) => i));
       final i = useStream(sm).data ?? 0;
-      return sca(
+      return scaff(
           dark.withAlpha(240),
           Icons.close,
           () => Navigator.pop(c),
           flex([
-            lb('${i~/60}:${i%60}', 52.0, light, 8),
-            lb('${(i * 0.0005).toStringAsFixed(3)}km', 44.0, green, 8),
+            txt('${i ~/ 60}:${i % 60}', 52.0, light, 8),
+            txt('${(i * 0.0005).toStringAsFixed(3)}km', 44.0, green, 8),
           ], 1, 2, 3));
     });
-home() => h((c) => sca(
+home() => h((c) => scaff(
       light,
       Icons.directions_run,
       () => Navigator.push(
@@ -104,14 +105,14 @@ list(it, c, h, l, b, p, s, x, o) => tr(
       itemCount: it.length + 1,
       itemBuilder: (c, i) => i == 0 ? header(h, l, p) : b(it[i - 1]),
     ));
-header(t, l, p) => cr(
+header(t, l, p) => box(
     flex([
-      lb(t, 2 * m1, dark, 8),
+      txt(t, 2 * m1, dark, 8),
       Spacer(),
       IconButton(
           onPressed: () =>
               p.animateToPage(l ? 0 : 1, curve: io, duration: du(300)),
-          icon: ic(l ? Icons.arrow_forward : Icons.arrow_back, dark))
+          icon: ico(l ? Icons.arrow_forward : Icons.arrow_back, dark))
     ], 0, 2, 2, l ? 1 : 0),
     m1,
     0.0,
@@ -123,18 +124,18 @@ overview(i, aa) => h((c) {
       });
       var a = useAnimation(ac);
       var color = clerp(((a - 0.5) * 2.0).abs());
-      return cr(
+      return box(
           flex([
-            lb(i["date"], m2, light, 3),
-            sp(),
+            txt(i["date"], m2, light, 3),
+            sp,
             line(i, a),
-            sp(),
+            sp,
             flex([
-              ic(Icons.place, color),
-              lb(i["place"], m2, light, 4),
+              ico(Icons.place, color),
+              txt(i["place"], m2, light, 4),
             ]),
-            lb(i["dst"], 36.0, light, 8),
-            lb(i["duration"], m1, color, 7),
+            txt(i["dst"], 36.0, light, 8),
+            txt(i["duration"], m1, color, 7),
           ], 1),
           0.0,
           m1,
@@ -143,11 +144,11 @@ overview(i, aa) => h((c) {
           dark,
           m1);
     });
-detail(item, a) => cr(
+detail(item, a) => box(
     flex(
-        (item["stats"] as List)
+        item["stats"]
             .expand(
-                (s) => [lb(s[0], m2, blue, 3), lb(s[1], m2, light, 8), sp()])
+                (s) => [txt(s[0], m2, blue, 3), txt(s[1], m2, light, 8), sp])
             .toList()
               ..add(graph(item, a)),
         1),
@@ -158,10 +159,10 @@ detail(item, a) => cr(
     sDark,
     m1);
 graph(item, a) => flex(
-    (item["wayp"] as List)
+    item["wayp"]
         .expand((x) => [
-              sp(),
-              cr(sp(), 0.0, 0.0, 4.0 + x * io.transform(a) * 34.0, 4.0,
+              sp,
+              box(sp, 0.0, 0.0, 4.0 + x * io.transform(a) * 34.0, 4.0,
                   clerp(x * a), 2.0)
             ])
         .skip(1)
@@ -170,7 +171,7 @@ graph(item, a) => flex(
     2,
     1);
 line(item, a) =>
-    cr(CustomPaint(painter: LP(item, io.transform(a))), 3.0, 0.0, 150.0, 150.0);
+    sb(150.0, 150.0, CustomPaint(painter: LP(item, io.transform(a))));
 
 class LP extends CustomPainter {
   var i;
@@ -181,17 +182,16 @@ class LP extends CustomPainter {
     np() => Paint()
       ..strokeWidth = 6
       ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeJoin = StrokeJoin.round;
+      ..strokeCap = StrokeCap.round;
     c
       ..translate((s.width / 2) - (p.getBounds().width / 2), 4)
       ..drawPath(p, np()..color = Colors.black26)
       ..translate(0, -4)
       ..drawPath(p, np()..color = blue);
-    var ps = i["points"] as List;
+    var ps = i["points"];
     var h = ps.lastWhere((x) => a > x[0] && a <= (x[0] + x[1]),
         orElse: () => ps.last);
-    var hp = Offset.lerp(of(h[2], h[3]), of(h[4], h[5]), (a - h[0]) / h[1]);
+    var hp = Offset.lerp(off(h[2], h[3]), off(h[4], h[5]), (a - h[0]) / h[1]);
     ha(x, r) => c.drawPath(
         p, np()..shader = halo(x, r, hp.dx / s.width, hp.dy / s.width, s));
     ha(green, 130.0);
